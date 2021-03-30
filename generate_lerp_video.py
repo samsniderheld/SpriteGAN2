8fec5e74381e18348b722ba01481c6058ffbe8bb
Samuel Snider-Held <samuel@mediamonks.com>
2021-03-30 09:51:37 -0400

    fixing args in a couple of files

--- generate_lerp_video.py
+++ generate_lerp_video.py
@@ -12,7 +12,7 @@ def parse_args():
   desc = "create a lerp video"
   parser = argparse.ArgumentParser(description=desc)
   parser.add_argument('--step', type=int, default=99000, help='The step to load the  model from ')
-  parser.add_argument('--numLerps', type=int, default=10, help='How many lerps to do')
+  parser.add_argument('--num_lerps', type=int, default=10, help='How many lerps to do')
   parser.add_argument('--noise_dim', type=int, default=512, help='The size of the latent vector')
   parser.add_argument('--img_dim', type=int, default=256, help='The dimension of the image')
 
@@ -26,23 +26,26 @@ else:
 
 args = parse_args()
 
-numLerps = args.numLerps
-
 idx=0
 
-generator = make_style_gan_generator(256, 512)
-generator.load_weights("SavedModels/generator_weights_at_step_{}.h5".format(99000))
+num_lerps = args.num_lerps
+noise_dim = args.noise_dim
+img_dim = args.img_dim
+step = args.step
 
-noise_vector_1 = noise(1,512)
-noise_image_1 = noise_image(1,256)
+generator = make_style_gan_generator(img_dim, noise_dim)
+generator.load_weights("SavedModels/generator_weights_at_step_{}.h5".format(step))
 
-noise_vector_2 = noise(1,512)
-noise_image_2 = noise_image(1,256)
+noise_vector_1 = noise(1,noise_dim)
+noise_image_1 = noise_image(1,img_dim)
+
+noise_vector_2 = noise(1,noise_dim)
+noise_image_2 = noise_image(1,img_dim)
 
 
 linX = list(np.linspace(0, 1, 50))
 
-for i in tqdm(range(0,numLerps)):
+for i in tqdm(range(0,num_lerps)):
 
   for x in linX:
 
@@ -51,7 +54,7 @@ for i in tqdm(range(0,numLerps)):
     #use a linear interpolater 
     lerped_vector = noise_vector_1 * (1-x) + noise_vector_2 * (x)
 
-    noise_vector_list = [lerped_vector] * int(log2(256) - 1)
+    noise_vector_list = [lerped_vector] * int(log2(img_dim) - 1)
 
     lerped_noise_image = noise_image_1 * (1-x) + noise_image_2 * (x)
 
@@ -66,5 +69,5 @@ for i in tqdm(range(0,numLerps)):
   noise_vector_1 = noise_vector_2
   noise_image_1 = noise_image_2
 
-  noise_vector_2 = noise(1,512)
-  noise_image_2 = noise_image(1,256)
+  noise_vector_2 = noise(1,noise_dim)
+  noise_image_2 = noise_image(1,img_dim)
--- test_GAN_generation.py
+++ test_GAN_generation.py
@@ -24,7 +24,7 @@ def parse_args():
   parser = argparse.ArgumentParser(description=desc)
   parser.add_argument('--data_dir', type=str, default="Data/SaganRaw/", help='The directory that holds the image data')
   parser.add_argument('--step', type=int, default=99000, help='The step to load the  model from ')
-  parser.add_argument('--noise_dim', type=int, default=512, help='The size of the latent vector')
+  parser.add_argument('--noise_dim', type=int, default=noise_dim, help='The size of the latent vector')
   parser.add_argument('--img_dim', type=int, default=256, help='The dimension of the image')
   parser.add_argument('--num_imgs_to_test', type=int, default=10, help='How many images are we going to test')
   parser.add_argument('--data_step', type=int, default=1, help='every nth data sample to skip')
@@ -61,8 +61,8 @@ dataset = []
 print("Generate Test Images")
 
 for i in tqdm(range(numImages)):
-  noiseVectorList = [noise(1,512)] * int(log2(256) - 1)
-  noiseImage = noise_image(1,256)
+  noiseVectorList = [noise(1,noise_dim)] * int(log2(img_dim) - 1)
+  noiseImage = noise_image(1,img_dim)
   image = generator.predict(noiseVectorList + [noiseImage], batch_size = 1)
   image = cv2.resize(image[0], dsize=(224, 224), interpolation=cv2.INTER_NEAREST)
   dataset.append(image)
